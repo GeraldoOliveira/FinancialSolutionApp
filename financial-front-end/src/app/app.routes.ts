fa91f77330f8193fb4a261dbfa8c6c1af7636b99
@GeraldoOliveira <gevo.neto@hotmail.com>
2025-09-19 15:44:36 -0300

    Adicionado login e register

--- financial-front-end/src/app/app.routes.ts
+++ financial-front-end/src/app/app.routes.ts
@@ -2,5 +2,9 @@ import { Routes } from '@angular/router';
 
 export const routes: Routes = [
     { path: '', redirectTo: '/home', pathMatch: 'full' },
-    { path: 'home', loadComponent: () => import('./navigation/home/home').then(m => m.Home) },
+    // Correto: importa a classe do componente diretamente
+    { path: 'home', loadComponent: () => import('./navigation/home/home').then(c => c.Home) },
+    { path: 'register', loadComponent: () => import('./account/register/register').then(c => c.Register) },
+    { path: 'login', loadComponent: () => import('./account/login/login').then(c => c.Login) }
+
 ];
