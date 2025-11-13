b35e3cdb001e0f3d88134b9a33de15c68bfaa33a
Geraldo Vitório de Oliveira Neto <gevo.neto@hotmail.com>
2025-11-12 22:57:35 -0300

    Add ngx-spinner loading indicator to expense list
    Integrated ngx-spinner to display a loading animation on the expense list page. Updated dependencies and styles to support ngx-spinner, and modified the component to show the spinner on initialization. Also added new sample expense data to db.json.

--- financial-front-end/src/app/app.config.ts
+++ financial-front-end/src/app/app.config.ts
@@ -2,7 +2,7 @@ import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZonelessC
 import { provideRouter } from '@angular/router';
 import { provideHttpClient } from '@angular/common/http'; 
 
-import { provideToastr } from 'ngx-toastr'
+import { provideToastr } from 'ngx-toastr';
 
 import { routes } from './app.routes';
 import { provideAnimations } from '@angular/platform-browser/animations';
@@ -14,6 +14,7 @@ export const appConfig: ApplicationConfig = {
     provideRouter(routes),
     provideHttpClient(),
     provideToastr(),
-    provideAnimations()
+    provideAnimations(),
+
   ]
 };
--- financial-front-end/src/app/features/expense/components/expense-list/expense-list.ts
+++ financial-front-end/src/app/features/expense/components/expense-list/expense-list.ts
@@ -5,6 +5,7 @@ import { CurrencyPipe } from '@angular/common';
 import { Expense } from '../../../../shared/models/expense-transaction'
 import { ExpenseService } from '../../services/expense.service';
 import { toSignal } from '@angular/core/rxjs-interop';
+import { NgxSpinnerModule, NgxSpinnerService } from 'ngx-spinner';
 
 @Pipe({
   name: 'responsibleName',
@@ -27,7 +28,7 @@ export class ResponsibleNamePipe implements PipeTransform {
 
 @Component({
   selector: 'app-expense-list',
-  imports: [RouterLink, ResponsibleNamePipe, CurrencyPipe],
+  imports: [RouterLink, ResponsibleNamePipe, CurrencyPipe, NgxSpinnerModule],
   templateUrl: './expense-list.html',
   styleUrl: './expense-list.css',
   providers: []
@@ -36,7 +37,9 @@ export class ExpenseList {
   public transactionsSignal: Signal<Expense[]>
   public errorMessageSignal = signal('');
 
-  constructor(private expenseTransactionService: ExpenseService) {
+  constructor(private expenseTransactionService: ExpenseService,
+              private spinner: NgxSpinnerService
+              ) {
 
     this.transactionsSignal = toSignal(
       this.expenseTransactionService.getAllTransactions(),
@@ -52,6 +55,9 @@ export class ExpenseList {
   }
 
   ngOnInit() {
-
+    this.spinner.show();
+    setTimeout(() => {
+      this.spinner.hide();
+    }, 2000);
   }
 }
